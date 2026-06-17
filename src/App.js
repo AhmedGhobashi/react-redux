403c5c1a56cf8f4e2f4feae4ba558c58e6183b12
AhmedGhobashi <ahmed.osamaa200000@gmail.com>
2026-06-17 17:20:53 +0300

    react-redux <3 simple and easy until now

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {useSelector, useDispatch} from 'react-redux'
 
 function App() {
+  const dispatch = useDispatch();
+
+  const state = useSelector((state)=> {
+    return state.value});
+    
+  const showtoggleState = useSelector((state)=> state.toggle);
+
+  const increase = ()=>{
+    const action = {type: "increase", payload: {number:5}};
+    dispatch(action);
+  }
+
+    const decrease = () => {
+      const action = { type: "decrease"};
+      dispatch(action);
+    };
+
+    const showCounter = ()=>{
+      dispatch ({type: "toggleCounter"});
+    }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Redux Basic</h1>
+      {showtoggleState && (
+        <>
+
+<div className="counter">Counter: {state}</div>
+<div>
+  <button className="btn" onClick={increase}>increase +</button>
+  <button className="btn" onClick={decrease}>decrease -</button>
+</div>
+        </>
+      )}
+      
+      <div>
+        <button className="btn" onClick={showCounter}>Hide/Show Counter Number</button>
+      </div>
+
     </div>
   );
 }
---
+++ src/store/index.js
@@ -0,0 +1,26 @@
+import {createStore} from 'redux';
+
+
+const initState = {value:0, toggle: true}
+
+const counterReducer =(state = initState, action)=>{
+    if(action.type === 'increase'){
+        return {...state, value: state.value + action.payload.number}
+    }
+
+    if (action.type === "decrease") {
+          return { ...state, value: state.value - 1};
+    }
+
+    if(action.type === "toggleCounter"){
+        return {...state, toggle: !state.toggle}
+    }
+    return state;
+}
+
+//app init >> action(none) >> return state (initState) >> value=0
+
+
+
+const store = createStore(counterReducer);
+export default store;
